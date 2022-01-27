ef5af49f94a36fe9061071300765b0fe8d906434
Michael Yip <michael@poyu.co.uk>
2022-01-27 19:49:45 +0000

    Initialise the current document with new added artifact settings

--- src/setup.ts
+++ src/setup.ts
@@ -87,6 +87,74 @@ function getSettingsSheet() {
       }
     } else {
       settingsSheet.getRange("H1").setValue(SCRIPT_VERSION);
+      /* Check migration for settings required */
+      var bannerSettings = LOG_RANGES[SHEET_NAME_ARTIFACT_LOG];
+      var isToggled = settingsSheet.getRange(bannerSettings['range_toggle']).getValue();
+      if(isToggled == ""){
+        // Migration step required, missing Artifact toggle
+        settingsSheet.getRange(13,4,1,2).setBorder(false, false, false, false, false, false).breakApart();
+        settingsSheet.getRange("D13").setFontSize(10).setFontWeight("bold").setHorizontalAlignment("center").setValue(SHEET_NAME_MORA_LOG);
+        settingsSheet.getRange("E13").setFontSize(10).setFontWeight(null).setHorizontalAlignment("center").setValue(settingsSheet.getRange("E12").getValue());
+        settingsSheet.getRange("D12").setFontSize(10).setFontWeight("bold").setHorizontalAlignment("center").setValue(SHEET_NAME_ARTIFACT_LOG);
+        settingsSheet.getRange("E12").setFontSize(10).setFontWeight(null).setHorizontalAlignment("center").setValue('NOT DONE');
+        settingsSheet.getRange(14,4,2,2).breakApart();
+
+        settingsSheet.getRange(14,4,1,2).setBorder(true, false, false, false, false, false, "black", SpreadsheetApp.BorderStyle.SOLID).mergeAcross();
+        settingsSheet.getRange("D14").setFontColor("black").setFontSize(11).setFontWeight("bold").setHorizontalAlignment("center").setValue('Auto Import');
+        settingsSheet.getRange(15,4,1,2).mergeAcross();
+        settingsSheet.getRange("D15").setFontColor("red").setFontSize(10).setFontWeight("bold").setHorizontalAlignment("center").setValue('Using Genshin Impact Feedback URL to call API with AUTH_KEY');
+
+        settingsSheet.getRange("D22").setFontSize(10).setFontWeight("bold").setHorizontalAlignment("center").setValue(SHEET_NAME_ARTIFACT_LOG);
+        settingsSheet.getRange("E22").setFontSize(10).setBackground("white").insertCheckboxes().setValue(true);
+        settingsSheet.getRange("D29").setFontSize(10).setFontWeight("bold").setHorizontalAlignment("center").setValue(SHEET_NAME_ARTIFACT_LOG);
+
+        // set data validation
+        var listOfSheets = [
+          SHEET_NAME_DASHBOARD,
+          SHEET_NAME_README,
+          SHEET_NAME_CHANGELOG,
+          SHEET_NAME_SETTINGS,
+          SHEET_NAME_PRIMOGEM_MONTHLY_REPORT,
+          SHEET_NAME_PRIMOGEM_YEARLY_REPORT,
+          SHEET_NAME_PRIMOGEM_LOG,
+          SHEET_NAME_CRYSTAL_MONTHLY_REPORT,
+          SHEET_NAME_CRYSTAL_YEARLY_REPORT,
+          SHEET_NAME_CRYSTAL_LOG,
+          SHEET_NAME_RESIN_MONTHLY_REPORT,
+          SHEET_NAME_RESIN_YEARLY_REPORT,
+          SHEET_NAME_RESIN_LOG,
+          SHEET_NAME_MORA_MONTHLY_REPORT,
+          SHEET_NAME_MORA_YEARLY_REPORT,
+          SHEET_NAME_MORA_LOG,
+          SHEET_NAME_ARTIFACT_MONTHLY_REPORT,
+          SHEET_NAME_ARTIFACT_YEARLY_REPORT,
+          SHEET_NAME_ARTIFACT_LOG
+        ];
+        var rule = SpreadsheetApp.newDataValidation().requireValueInList(listOfSheets, true).build();
+        settingsSheet.getRange(11,2,20,1).setBackground("white").setFontSize(10).setFontWeight(null).setHorizontalAlignment("center").setDataValidation(rule);;
+        settingsSheet.getRange("A28").setFontSize(10).setFontWeight("bold").setHorizontalAlignment("center").setValue(18);
+        settingsSheet.getRange("A29").setFontSize(10).setFontWeight("bold").setHorizontalAlignment("center").setValue(19);
+        settingsSheet.getRange("A30").setFontSize(10).setFontWeight("bold").setHorizontalAlignment("center").setValue(20);
+        
+        settingsSheet.getRange("B27").setValue(SHEET_NAME_ARTIFACT_MONTHLY_REPORT);
+        settingsSheet.getRange("B28").setValue(SHEET_NAME_ARTIFACT_YEARLY_REPORT);
+        settingsSheet.getRange("B29").setValue(SHEET_NAME_ARTIFACT_LOG);
+        // Load Artifact Log Sheet if missing
+        var artifactLogSheet = SpreadsheetApp.getActive().getSheetByName(SHEET_NAME_ARTIFACT_LOG);
+        if (!artifactLogSheet) {
+          if (!sheetSource) {
+            sheetSource = SpreadsheetApp.openById(SHEET_SOURCE_ID);
+          }
+          var sheetArtifactLogSource = sheetSource.getSheetByName(SHEET_NAME_ARTIFACT_LOG);
+          artifactLogSheet = sheetArtifactLogSource.copyTo(SpreadsheetApp.getActiveSpreadsheet());
+          artifactLogSheet.setName(SHEET_NAME_ARTIFACT_LOG);
+        }
+        // Remove old Dashboard if exist
+        var removeDashboardSheet = SpreadsheetApp.getActive().getSheetByName(SHEET_NAME_DASHBOARD);
+        if (removeDashboardSheet) {
+          SpreadsheetApp.getActiveSpreadsheet().deleteSheet(removeDashboardSheet);
+        }
+      }
     }
     var dashboardSheet = SpreadsheetApp.getActive().getSheetByName(SHEET_NAME_DASHBOARD);
     if (!dashboardSheet) {
